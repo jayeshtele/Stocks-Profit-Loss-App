4c1f916e0468f52acc169664739cfb8224a74557
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-12 22:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const buy = document.querySelector("#buy");
 const sell = document.querySelector("#sell");
 const quantity = document.querySelector("#quantity");
 const tellme = document.querySelector(".btn");
+const output = document.querySelector(".output");
 
-tellme.addEventListener("click", ()=>{
-    console.log(buy.value)
-    console.log(sell.value)
-    console.log(quantity.value)
+tellme.addEventListener("click", () => {
+
+    let bp = Number(buy.value)
+    let sp = Number(sell.value)
+    let qty = Number(quantity.value)
+
+    if (sp > bp) {
+        let profit = (sp - bp) * qty;
+        let profitPercent = (profit / bp) * 100;
+        output.innerHTML = `Hey the Profit is ${profit} Rupees which is ${profitPercent}% from buying point`;
+    } else if (bp > sp) {
+        let loss = (bp - sp) * qty;
+        let lossPercent = (loss / bp) * 100;
+        output.innerHTML = `Hey the Loss is ${loss} Rupees which is ${lossPercent}% from buying point`;
+
+    } else {
+        output.innerHTML = "No Changes from your buying price";
+    }
+
 });
