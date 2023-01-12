8935b0a023d2a3488730a1a6b9a4d594e75ae271
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-12 22:10:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const buy = document.querySelector("#buy");
 const sell = document.querySelector("#sell");
 const quantity = document.querySelector("#quantity");
+const tellme = document.querySelector(".btn");
+
+tellme.addEventListener("click", ()=>{
+    console.log(buy.value)
+    console.log(sell.value)
+    console.log(quantity.value)
+});
